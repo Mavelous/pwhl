fd1a55f5817dfc23e21f7e3555a72275e7bd3284
Mike Vitale <github@mikevitale.com>
2024-01-08 21:13:41 -0600

    Make main() work again.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,30 @@
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     alias(libs.plugins.jvm)
 
-    // Apply the application plugin to add support for building a CLI application in Java.
     application
 
     kotlin("plugin.serialization") version "1.9.22"
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
-    // This dependency is used by the application.
     implementation(libs.guava)
-    implementation("it.skrape:skrapeit:1.3.0-alpha.2")
+    implementation("it.skrape:skrapeit:1.2.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
     implementation("com.google.code.gson:gson:2.9.0")
 }
 
 testing {
     suites {
-        // Configure the built-in test suite
         val test by getting(JvmTestSuite::class) {
-            // Use Kotlin Test test framework
             useKotlinTest("1.9.20")
         }
     }
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
@@ -39,6 +32,5 @@ java {
 }
 
 application {
-    // Define the main class for the application.
     mainClass.set("us.mavelo.AppKt")
 }
