00e41b30a1d80c05ed563834c8d03255c10652f0
Mike Vitale <github@mikevitale.com>
2024-02-16 19:34:53 -0600

    Improve testing.

--- src/test/kotlin/us/mavelo/pwhl/json/common/PropTest.kt
+++ src/test/kotlin/us/mavelo/pwhl/json/common/PropTest.kt
@@ -1,16 +1,52 @@
 package us.mavelo.pwhl.json.common
 
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 class PropTest {
 	@Test
-	fun canConstruct() {
+	fun isSerializable() {
+		assertEquals(1, Prop::class.annotations.size)
+		assertNotNull(Prop::class.annotations.find { it is Serializable })
+	}
+
+	@Test
+	fun canConstructProp() {
+		val prop: Prop = Json.decodeFromString("""{
+	"name": {
+		"playerLink": "100",
+		"seoName": "Natalie Spooner"
+	},
+	"active": {
+		"active": "1"
+	},
+	"rookie": {
+		"rookie": "0"
+	},
+	"team_code": {
+		"teamLink": "6"
+	}
+}
+""")
+
+		assertEquals("Natalie Spooner", prop.name!!.seoName)
+		assertEquals("1", prop.active!!.active)
+		assertEquals("0", prop.rookie!!.rookie)
+		assertEquals("6", prop.teamCode!!.teamLink)
+	}
+
+	@Test
+	fun canConstructPropFromFile() {
 		val jsonContent: String = this::class.java.getResource("/json/common/prop.json")?.readText(Charsets.UTF_8) ?: ""
 
 		val prop: Prop = Json.decodeFromString<Prop>(jsonContent)
 
-		assertNotNull(prop)
+		assertEquals("Natalie Spooner", prop.name!!.seoName)
+		assertEquals("1", prop.active!!.active)
+		assertEquals("0", prop.rookie!!.rookie)
+		assertEquals("6", prop.teamCode!!.teamLink)
 	}
 }
