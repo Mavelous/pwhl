64d04559b505bc1e26461bafb607a6d97fe9e257
Mike Vitale <github@mikevitale.com>
2025-11-22 12:28:45 -0600

    Add Team shortName.

--- src/main/kotlin/us/mavelo/pwhl/Team.kt
+++ src/main/kotlin/us/mavelo/pwhl/Team.kt
@@ -1,12 +1,12 @@
 package us.mavelo.pwhl
 
-enum class Team(val teamNum: Int, val topColor: String = "#fff", val bottomColor: String = "#000") {
-	BOSTON(1, "#164E36", "#C3C6CC"),
-	MINNESOTA(2, "#250E62", "#A77BCA"),
-	MONTREAL(3, "#651517"),
-	NEW_YORK(4, "#18B196", "#151E38"),
-	OTTAWA(5, "#BC1F1E", "#383334"),
-	TORONTO(6, "#1A75BD"),
-	VANCOUVER(9, "#0F4777", "#EEE9D8"),
-	SEATTLE(8, "#0A5258", "#EEE9D8"),
+enum class Team(val teamNum: Int, val shortName: String, val topColor: String = "#fff", val bottomColor: String = "#000") {
+	BOSTON(1, "BOS", "#164E36", "#C3C6CC"),
+	MINNESOTA(2, "MIN", "#250E62", "#A77BCA"),
+	MONTREAL(3, "MTL", "#651517"),
+	NEW_YORK(4, "NY", "#18B196", "#151E38"),
+	OTTAWA(5, "OTT", "#BC1F1E", "#383334"),
+	TORONTO(6, "TOR", "#1A75BD"),
+	VANCOUVER(9, "VAN", "#0F4777", "#EEE9D8"),
+	SEATTLE(8, "SEA", "#0A5258", "#EEE9D8"),
 }
--- src/test/kotlin/us/mavelo/pwhl/TeamTest.kt
+++ src/test/kotlin/us/mavelo/pwhl/TeamTest.kt
@@ -12,6 +12,7 @@ class TeamTest {
 	@Test
 	fun boston() {
 		assertEquals(1, Team.BOSTON.teamNum)
+		assertEquals("BOS", Team.BOSTON.shortName)
 		assertEquals("#164E36", Team.BOSTON.topColor)
 		assertEquals("#C3C6CC", Team.BOSTON.bottomColor)
 		assertEquals("boston", Team.BOSTON.toString().lowercase())
@@ -20,6 +21,7 @@ class TeamTest {
 	@Test
 	fun minnesota() {
 		assertEquals(2, Team.MINNESOTA.teamNum)
+		assertEquals("MIN", Team.MINNESOTA.shortName)
 		assertEquals("#250E62", Team.MINNESOTA.topColor)
 		assertEquals("#A77BCA", Team.MINNESOTA.bottomColor)
 		assertEquals("minnesota", Team.MINNESOTA.toString().lowercase())
@@ -28,6 +30,7 @@ class TeamTest {
 	@Test
 	fun montreal() {
 		assertEquals(3, Team.MONTREAL.teamNum)
+		assertEquals("MTL", Team.MONTREAL.shortName)
 		assertEquals("#651517", Team.MONTREAL.topColor)
 		assertEquals("#000", Team.MONTREAL.bottomColor)
 		assertEquals("montreal", Team.MONTREAL.toString().lowercase())
@@ -36,6 +39,7 @@ class TeamTest {
 	@Test
 	fun newYork() {
 		assertEquals(4, Team.NEW_YORK.teamNum)
+		assertEquals("NY", Team.NEW_YORK.shortName)
 		assertEquals("#18B196", Team.NEW_YORK.topColor)
 		assertEquals("#151E38", Team.NEW_YORK.bottomColor)
 		assertEquals("new_york", Team.NEW_YORK.toString().lowercase())
@@ -44,6 +48,7 @@ class TeamTest {
 	@Test
 	fun ottawa() {
 		assertEquals(5, Team.OTTAWA.teamNum)
+		assertEquals("OTT", Team.OTTAWA.shortName)
 		assertEquals("#BC1F1E", Team.OTTAWA.topColor)
 		assertEquals("#383334", Team.OTTAWA.bottomColor)
 		assertEquals("ottawa", Team.OTTAWA.toString().lowercase())
@@ -52,6 +57,7 @@ class TeamTest {
 	@Test
 	fun toronto() {
 		assertEquals(6, Team.TORONTO.teamNum)
+		assertEquals("TOR", Team.TORONTO.shortName)
 		assertEquals("#1A75BD", Team.TORONTO.topColor)
 		assertEquals("#000", Team.TORONTO.bottomColor)
 		assertEquals("toronto", Team.TORONTO.toString().lowercase())
@@ -60,6 +66,7 @@ class TeamTest {
 	@Test
 	fun vancouver() {
 		assertEquals(9, Team.VANCOUVER.teamNum)
+		assertEquals("VAN", Team.VANCOUVER.shortName)
 		assertEquals("#0F4777", Team.VANCOUVER.topColor)
 		assertEquals("#EEE9D8", Team.VANCOUVER.bottomColor)
 		assertEquals("vancouver", Team.VANCOUVER.toString().lowercase())
@@ -68,6 +75,7 @@ class TeamTest {
 	@Test
 	fun seattle() {
 		assertEquals(8, Team.SEATTLE.teamNum)
+		assertEquals("SEA", Team.SEATTLE.shortName)
 		assertEquals("#0A5258", Team.SEATTLE.topColor)
 		assertEquals("#EEE9D8", Team.SEATTLE.bottomColor)
 		assertEquals("seattle", Team.SEATTLE.toString().lowercase())
